c28b2b238a51e05c2123ead71d6aca7d6c867463
Eduardo Secolin <dusecolin@yahoo.com.br>
2018-04-22 12:29:55 -0300

    Iniciando a tela principal..projeto ainda sem objetivo

--- CESSistemas/MainWindow.xaml.cs
+++ CESSistemas/MainWindow.xaml.cs
@@ -21,5 +21,21 @@ namespace CESSistemas {
         public MainWindow() {
             InitializeComponent();
         }
+
+        // botao de logout
+        private void btnLogout_Click(object sender, RoutedEventArgs e) {
+            Application.Current.Shutdown();
+        }
+
+        // abrir o menu
+        private void btnAbrirMenu_Click(object sender, RoutedEventArgs e) {
+            btnAbrirMenu.Visibility = Visibility.Collapsed;
+            btnFecharMenu.Visibility = Visibility.Visible;
+        }
+        // fechar o menu
+        private void btnFecharMenu_Click(object sender, RoutedEventArgs e) {
+            btnAbrirMenu.Visibility = Visibility.Visible;
+            btnFecharMenu.Visibility = Visibility.Collapsed;
+        }
     }
 }
