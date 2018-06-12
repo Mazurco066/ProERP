626aaf114bdbdb617ad14d3172d089dc3b49f746
Eduardo Secolin <dusecolin@yahoo.com.br>
2018-06-11 22:56:59 -0300

    Mudando o design de windows form para wpf e alterando banco de dados de mysql para sql server

---
+++ CESSistemas/SplahScreen.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace CESSistemas
+{
+    /// <summary>
+    /// Lógica interna para SplahScreen.xaml
+    /// </summary>
+    public partial class SplahScreen : Window
+    {
+
+        private const int TEMP = 1000;
+        public SplahScreen()
+        {
+            InitializeComponent();
+            //carregarprogressBar();
+        }
+
+        private delegate void ProgressBarDelegate();
+
+        private void criarConstrucao() {
+            PB.IsIndeterminate = false;
+            PB.Maximum = TEMP;
+            PB.Value = 0;
+
+            for (int i = 0; i < TEMP; i++) {
+                PB.Dispatcher.Invoke(new ProgressBarDelegate(UpdateProgress), DispatcherPriority.Background);
+            }
+        }
+        private void UpdateProgress() {
+            PB.Value += 1;
+        }
+
+
+        private void carregarprogressBar() {
+            int cont = 0;
+            while (cont < 5) {
+
+                criarConstrucao();
+                cont++;
+            }
+            try {
+                if (cont >= 5) {
+                    Login janela = new Login();
+                    janela.Show();
+                    this.Hide();
+                    Close();
+                }
+            }
+            catch (Exception a) {
+                MessageBox.Show(a.Message);
+            }
+        }
+
+        private void Window_Activated(object sender, EventArgs e) {
+            carregarprogressBar();
+        }
+    }
+}
