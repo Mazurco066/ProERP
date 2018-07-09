d6b918fc809808a896100b40e4e390f6e7869cf8
Gabriel Ribeiro <mazurco066@gmail.com>
2018-07-09 18:14:47 -0300

    Tela de Fornecedores - Finalizada

--- CESSistemas/View/Components/UserControlSupplier.xaml.cs
+++ CESSistemas/View/Components/UserControlSupplier.xaml.cs
@@ -62,9 +62,271 @@ namespace Promig.View.Components {
             RefreshGrid();
         }
 
+        //Evento de autopreencher cep
+        private void cepEdit_PreviewKeyUp(object sender, KeyEventArgs e) {
+            if (e.Key == Key.Enter) {
+                string cep = cepEdit.Text.Replace("-", "").Replace("_", "");
+                Adress adress = WSAdress.GetAdress(cep);
+                //Recuperando dados gerais
+                AdressEdit.Text = adress.street;
+                NeighboorhoodEdit.Text = adress.neighborhood;
+                CityEdit.Text = adress.city;
+                //Recuperando Estado
+                int index = -1;
+                foreach (ComboBoxItem item in cbState.Items) {
+                    index++;
+                    if (item.Content.Equals(adress.UF)) break;
+                }
+                cbState.SelectedIndex = index;
+            }
+        }
+
+        //Evento ao clicar em algum funcionário
+        private void dgSuppliers_SelectionChanged(object sender, SelectionChangedEventArgs args) {
+
+            //Verificando se ha seleção feita
+            if (dgSuppliers.SelectedItems.Count > 0) {
+
+                //Recuperando dados do cliente selecionado
+                Supplier source = dgSuppliers.SelectedItem as Supplier;
+                aux = dao.GetSupplierData(source.id);
+
+                //Preenchendo campos (Comboboxes)
+                if (aux.IsActive()) cbActive.SelectedIndex = 0; else cbActive.SelectedIndex = 1;
+
+                //Recuperando Estado
+                int index = -1;
+                foreach (ComboBoxItem item in cbState.Items) {
+                    index++;
+                    if (item.Content.Equals(aux.adress.UF)) break;
+                }
+                cbState.SelectedIndex = index;
+
+                //Preenchendo campos (Textboxes)
+                NameEdit.Text = aux.name;
+                cnpjEdit.Text = aux.cnpj;
+                AdressEdit.Text = aux.adress.street;
+                NeighboorhoodEdit.Text = aux.adress.neighborhood;
+                cepEdit.Text = aux.adress.CEP;
+                NumberEdit.Text = aux.adress.number;
+                CityEdit.Text = aux.adress.city;
+                phone1Edit.Text = aux.resPhone;
+                phone2Edit.Text = aux.cellPhone;
+
+                //Definindo ação como nula
+                actionIndex = -1;
+
+                //Desabiilitando campos e preenchendo com dados
+                BlockFields();
+            }
+        }
+
+        //Evento para botão adicionar
+        private void btnAdicionar_Click(object sender, RoutedEventArgs e) {
+            //Habilitando campos para inserção
+            actionIndex = 1;
+            ClearFields();
+            EnableFields();
+        }
+
+        //Evento para botão editar
+        private void btnEditar_Click(object sender, RoutedEventArgs e) {
+
+            //Verificando se ha funcionario selecionada
+            if (dgSuppliers.SelectedItems.Count > 0) {
+
+                //Desbloqueando os campos
+                actionIndex = 2;
+                EnableFields();
+            }
+            else {
+                MessageBox.Show(
+                        "Nenhum fornecedor foi selecionado! Selecione um para prosseguir...",
+                        "Validação",
+                        MessageBoxButton.OK,
+                        MessageBoxImage.Warning
+                    );
+            }
+        }
+
+        //Evento para botão cancelar
+        private void btnCancelar_Click(object sender, RoutedEventArgs e) {
+
+            //Bloqueando campos e resetando transação
+            BlockFields();
+            ClearFields();
+            actionIndex = -1;
+        }
+
+        //Evento para botão salvar
+        private void btnSalvar_Click(object sender, RoutedEventArgs e) {
+
+            //Verificando qual ação sera realizada
+            switch (actionIndex) {
+
+                case 1:
+                    AddSupplier();
+                    break;
+
+                case 2:
+                    EditSupplier();
+                    break;
+
+                default:
+                    MessageBox.Show(
+                            "Nenhuma Operação Selecionada!",
+                            "Erro no preenchimento do formulário",
+                            MessageBoxButton.OK,
+                            MessageBoxImage.Warning
+                        );
+                    break;
+
+            }
+        }
+
         #endregion
 
         #region Data-Gathering
+
+        private void AddSupplier() {
+            if (IsValidFields()) {
+                string cnpj = cnpjEdit.Text.Replace(".", "").Replace("-", "").Replace("_", ".").Replace("/", "");
+                string cep = cepEdit.Text.Replace("-", "").Replace("_", "");
+                if (Validator.IsCnpj(cnpj)) {  //Validando documento
+                    try {
+                        //Recuperando dados do funcionário
+                        Supplier sup = new Supplier();
+                        ComboBoxItem selected = cbState.Items[cbState.SelectedIndex] as ComboBoxItem;
+                        sup.adress.street = AdressEdit.Text;
+                        sup.adress.city = CityEdit.Text;
+                        sup.adress.neighborhood = NeighboorhoodEdit.Text;
+                        sup.adress.number = NumberEdit.Text;
+                        sup.adress.UF = selected.Content.ToString();
+                        sup.adress.CEP = cep;
+                        sup.name = NameEdit.Text;
+                        sup.cnpj = cnpj;
+                        sup.cellPhone = phone2Edit.Text;
+                        sup.resPhone = phone1Edit.Text;
+                        if (cbActive.SelectedIndex == 1) sup.Inactivate();  
+
+                        //Inserindo registro no banco
+                        dao.AddSupplier(sup);
+
+                        //Registrando log de alteração
+                        Model.Log added = new Model.Log();
+                        added.employe = _employe;
+                        added.action = "Fornecedor " + sup.name + " foi cadastrado no sistema!";
+                        logs.Register(added);
+
+                        //Atualizando grid e limpando campos de texto
+                        RefreshGrid();
+                        ClearFields();
+                        BlockFields();
+                        actionIndex = -1;
+                        aux = null;
+
+                    }
+                    catch (DatabaseInsertException err) {
+                        //Retornando mensagem de erro para usuário
+                        MessageBox.Show(
+                        err.Message,
+                        "Erro ao gravar dados",
+                        MessageBoxButton.OK,
+                        MessageBoxImage.Error);
+                    }
+                }
+                else {
+                    //Retornando mensagem de validação
+                    MessageBox.Show(
+                        "CPF Inválido",
+                        "Dados incorretos!",
+                        MessageBoxButton.OK,
+                        MessageBoxImage.Warning
+                    );
+                }
+            }
+            else {
+                //Mostrando alerta de validação
+                MessageBox.Show(
+                    "Há Campos Vazios",
+                    "Erro de Prenchimento de Formulário",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Warning
+                );
+            }
+        }
+
+        private void EditSupplier() {
+
+            //Verificando se campos estão preenchidos
+            if (IsValidFields()) {
+                string cnpj = cnpjEdit.Text.Replace(".", "").Replace("-", "").Replace("_", ".").Replace("/","");
+                string cep = cepEdit.Text.Replace("-", "").Replace("_", "");
+                if (Validator.IsCnpj(cnpj)) {  //Validando documentos
+                    try {
+
+                        //Recuperando dados do funcionário
+                        ComboBoxItem selected = cbState.Items[cbState.SelectedIndex] as ComboBoxItem;
+                        aux.adress.street = AdressEdit.Text;
+                        aux.adress.city = CityEdit.Text;
+                        aux.adress.neighborhood = NeighboorhoodEdit.Text;
+                        aux.adress.number = NumberEdit.Text;
+                        aux.adress.UF = selected.Content.ToString();
+                        aux.adress.CEP = cep;
+                        aux.name = NameEdit.Text;
+                        aux.cnpj = cnpj;
+                        aux.cellPhone = phone2Edit.Text;
+                        aux.resPhone = phone1Edit.Text;
+                        if (cbActive.SelectedIndex == 1) aux.Inactivate(); else aux.Activate();
+                      
+                        //Alterando registro no banco
+                        dao.EditSupplier(aux);
+
+                        //Registrando log de alteração
+                        Model.Log edited = new Model.Log();
+                        edited.employe = _employe;
+                        edited.action = "Fornecedor " + aux.GetName() + " com ID = " + aux.id + " sofreu alteração no sistema!";
+                        logs.Register(edited);
+
+                        //Atualizando grid e limpando campos de texto
+                        RefreshGrid();
+                        ClearFields();
+                        BlockFields();
+                        actionIndex = -1;
+                        aux = null;
+
+                    }
+                    catch (DatabaseEditException err) {
+
+                        //Retornando mensagem de erro para usuário
+                        MessageBox.Show(
+                        err.Message,
+                        "Erro ao gravar dados",
+                        MessageBoxButton.OK,
+                        MessageBoxImage.Error);
+                    }
+
+                }
+                else {
+                    //Retornando mensagem de validação
+                    MessageBox.Show(
+                        "CPF ou RG Inválido(s)",
+                        "Dados incorretos!",
+                        MessageBoxButton.OK,
+                        MessageBoxImage.Warning
+                    );
+                }
+            }
+            else {
+                //Mostrando alerta de validação
+                MessageBox.Show(
+                    "Há Campos Vazios",
+                    "Erro de Prenchimento de Formulário",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Warning
+                );
+            }
+        }
 
         #endregion
 
