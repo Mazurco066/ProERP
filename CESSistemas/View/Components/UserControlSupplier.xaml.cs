7ae50b1dcda589aa1d5cd53d0734e2d6a66873e7
Gabriel Ribeiro <mazurco066@gmail.com>
2018-07-09 01:07:48 -0300

    Preparando arquivos para Tela de Fucionarios

--- CESSistemas/View/Components/UserControlSupplier.xaml.cs
+++ CESSistemas/View/Components/UserControlSupplier.xaml.cs
@@ -4,12 +4,32 @@ namespace Promig.View.Components {
     
     public partial class UserControlSupplier : UserControl {
 
-        #region constructors
+        #region Header
+
+        #endregion
+
+        #region Constructors
 
         public UserControlSupplier() {
             InitializeComponent();
         }
 
-        #endregion constructors
+        #endregion
+
+        #region Events
+
+        #endregion
+
+        #region Data-Gathering
+
+        #endregion
+
+        #region Grid-Param
+
+        #endregion
+
+        #region Utils
+
+        #endregion
     }
 }
