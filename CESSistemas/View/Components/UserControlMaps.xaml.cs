c575c8a9a46dc29843e36d799cd2f154cc7da9ca
Gabriel Ribeiro <mazurco066@gmail.com>
2018-07-11 22:43:55 -0300

    Tentativa de implementar google maps no projeto

---
+++ CESSistemas/View/Components/UserControlMaps.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows.Controls;
+using System.Windows.Threading;
+using System.Threading;
+using System.Net;
+using System.Drawing;
+using System.IO;
+using Microsoft.Win32;
+using System.Xml.Linq;
+
+namespace Promig.View.Components {
+    
+    public partial class UserControlMaps : UserControl {
+
+        #region Fields
+        private XDocument geoDoc;
+        private SaveFileDialog saveDialog;
+        private string location;
+        private int zoom;
+        private string mapType;
+        private double lat;
+        private double lng;
+        #endregion
+
+        public UserControlMaps() {
+            InitializeComponent();
+        }
+
+        private void GeoSubData() {
+
+        }
+    }
+}
